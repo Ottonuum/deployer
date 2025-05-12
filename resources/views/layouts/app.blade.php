7b4029d94ea62f382990d557c40e9794959d5ea3
Henry Kalvi Kaalma <Dudetitan@gmail.com>
2025-05-12 10:56:01 +0300

    fix: help npm run build start working

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
         body {
             font-family: 'Nunito', sans-serif;
