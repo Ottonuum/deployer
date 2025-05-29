b46983abf66dcfe32b9c85da7fc2553fde53d0ee
Ottonuum <Nuumotto1@gmail.com>
2025-05-29 21:49:49 +0300

    comimit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        // Create test user
+        \App\Models\User::create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('password'),
         ]);
 
         $this->call([
